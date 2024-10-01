73b3d9218a8d78e51ee0cb2715b0f03f2f69643f
Ray Yeh <n11374268@qut.edu.au>
2024-10-01 13:46:06 +1000

    Initial commit

---
+++ as1/customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace as1
+{
+    public class Customer : user
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string EmailAddress { get; set; }
+        int phoneNumber { get; set; }
+        string companyName { get; set; }
+        string address { get; set; }
+        string contry { get; set; }
+
+        public Customer(string username, string password) : base(username, password)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
